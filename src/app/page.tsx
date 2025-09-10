3cfb38f25f128aa1602ef4b532aed7adec047d4f
joeljarvi <joeljarvi@live.se>
2025-09-10 09:20:59 +0200

    la till tarotbilder och pages-filstruktur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Chat from "./components/Chat";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <Chat />
-    </main>
+    <main className="flex min-h-screen items-center justify-center"></main>
   );
 }
